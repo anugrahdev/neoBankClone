349c9ff5097620516d4228a9009959071e3764d2
Anang Nugraha <anang.nugraha@allianz.co.id>
2024-07-06 13:22:56 +0700

    Initial Commit

---
+++ neoBankClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  neoBankClone
+//
+//  Created by Anang Nugraha on 06/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
